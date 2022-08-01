598721686ce938f47c92cc516dd8235831156078
sjtg <chipilej@gmail.com>
2022-08-01 12:32:27 +0200

    Created api for app

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,2 +1,9 @@
 class IndexController < ApplicationController
+    def index
+        @napsarates = NapsaRate.all
+    end
+    
+    def show
+        @napsarate = NapsaRate.find(params[:id])
+    end
 end
--- app/models/napsa_rate.rb
+++ app/models/napsa_rate.rb
@@ -1,2 +1,3 @@
 class NapsaRate < ApplicationRecord
+    
 end
